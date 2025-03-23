eb2b734df952937a93e0d2bb2d28f2dfda916d65
laodie666 <mhkjordan@gmail.com>
2025-03-22 22:47:57 -0400

    Changed to github and works now

--- extension/window.js
+++ extension/window.js
@@ -24,6 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
+    
     const readButton = document.getElementById('readButton');
     if (readButton) {
         readButton.addEventListener('click', function() {
@@ -34,22 +35,64 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function readTextField() {
-    // Find the Instagram message input field
-            chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-                chrome.scripting.executeScript({
-                    target: { tabId: tabs[0].id },
-                    func: function() {
-                        var inputField = document.querySelector('.x3jgonx');
+    // Find the discord message input field
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            func: function() {
+                // Select all chat message containers
+                const messageContainers = document.querySelectorAll('li.messageListItem__5126c');
+                const messages = [];
+                
+                var last_username = "";
+                messageContainers.forEach(container => {
+                    // Extract username
+                    const usernameElement = container.querySelector('.username_c19a55');
+                    var username = usernameElement ? usernameElement.textContent : 'Unknown';
+                    if (username != 'Unknown') {
+                        last_username = username;
+                    } else {
+                        username = last_username;
+                    }
 
-                        if (inputField) {
-                            // Extract the text inside the input field
-                            var typedMessage = inputField.innerText || inputField.value || "";
-                            chrome.runtime.sendMessage({ action: "getMessage", message: typedMessage });
-                        } else {
-                            chrome.runtime.sendMessage({ action: "getMessage", message: null });
-                        }
-                }  
-            });
+                    // Extract timestamp
+                    const timestampElement = container.querySelector('.timestamp_c19a55 time');
+                    const timestamp = timestampElement ? timestampElement.getAttribute('datetime') : null;
+
+                    // Extract message content
+                    const messageContentElement = container.querySelector('.markup__75297.messageContent_c19a55');
+                    const messageContent = messageContentElement ? messageContentElement.textContent : null;
+
+                    const imageContainer = container.querySelector('.loadingOverlay_af017a');
+                    const imageElement = imageContainer ? imageContainer.querySelector('img') : null;
+                    const imageSrc = imageElement ? imageElement.src : null;
+                    // Create message object
+                    const messageObject = {
+                        username: username,
+                        timestamp: timestamp,
+                        message: messageContent,
+                        imageSrc: imageSrc
+                    };
+                    messages.push(messageObject);
+                    
+
+
+                });
+                // Log the messages JSON object
+                 console.log(JSON.stringify(messages, null, 2));
+                 
+
+                // typing input field
+                var inputField = document.querySelector('span[data-slate-string="true"]');
+                console.log("inputField: " + inputField);
+                if (inputField) {
+                    // Extract the text inside the input field
+                    var typedMessage = inputField.innerText || inputField.value || "";
+                    chrome.runtime.sendMessage({ action: "getMessage", message: typedMessage });
+                } else {
+                    chrome.runtime.sendMessage({ action: "getMessage", message: null });
+                }
+            }  
+        });
     });
-
 }
