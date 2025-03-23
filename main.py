f9202511b24c2f5027b048f1ec36150d019d5f9b
Yoshiku Yuu <119818606+YoshikuYuu@users.noreply.github.com>
2025-03-23 01:50:17 -0400

    fix main.py

--- main.py
+++ main.py
@@ -1,35 +1,12 @@
 from flask import Flask, jsonify, render_template, request
 from gemini import generate_rizz
 
-app = Flask(__name__, static_url_path='/static')
-
-@app.route('/')
-@app.route('/wingman')
-def wingman():
-    return render_template('index.html')
+app = Flask(__name__)
 
 # Function that recieves data from the front end
 @app.route('/rizzify', methods=['POST'])
 def rizzify():
-    # data = request.get_json()
-
-    # Test data
-    data = {
-        "relationship": "friend",
-        "current_message": "Hey, how are you doing?",
-        "chat_history": [
-            {
-                "type": "text",
-                "sender": "user",
-                "content": "Hey, how are you doing?"
-            },
-            {
-                "type": "text",
-                "sender": "recipient",
-                "content": "I'm good, how about you?"
-            }
-        ]
-    }
+    data = request.get_json()
 
     relationship = data.get('relationship')
     if relationship is None:
@@ -49,5 +26,4 @@ def rizzify():
         return jsonify({"status": "success", "msg": return_msg})
 
 if __name__ == '__main__':
-    # app.run(port=8000, host="0.0.0.0", debug=True)
-    rizzify()
+    app.run(host='0.0.0.0', port=81)
